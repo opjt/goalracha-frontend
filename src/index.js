8f120c95990af1278658c0951efa168459071949
HaneulHong <hongha303@naver.com>
2024-02-29 15:25:35 +0900

    creacted common layout

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './LAYOUT/footer';
 import reportWebVitals from './reportWebVitals';
+import Userheader from './LAYOUT/userheader'
+import Adminheader from './LAYOUT/adminheader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Adminheader />
   </React.StrictMode>
 );
 
