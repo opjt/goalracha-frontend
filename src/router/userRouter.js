14a5707d2d645a0cf0b00a5d0a87a2ca53531ee3
opjt <jtpark1957@gmail.com>
2024-03-15 20:55:19 +0900

    reserve-list router modify

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -5,7 +5,7 @@ const Logout = lazy(() => import("../pages/member/user/UserLogoutPage"))
 const KakaoRedirect = lazy(() => import("../pages/member/user/KakaoRedirectPage"))
 const Join = lazy(() => import("../pages/member/user/UserJoinPage"))
 const UserMyPage = lazy(() => import("../pages/member/user/UserMyPage"))
-const ReserveList = lazy(() => import("../pages/member/user/UserReserveListPage"))
+// const ReserveList = lazy(() => import("../pages/member/user/UserReserveListPage"))
 
 const memberRouter = () => {
 
@@ -29,11 +29,12 @@ const memberRouter = () => {
         {
             path: "mypage",
             element: <Suspense fallback={Loading}><UserMyPage /></Suspense>
-        }, 
-        {
-            path: "reserve-list",
-            element: <Suspense fallback={Loading}><ReserveList /></Suspense>
-        },
+        }
+        // , 
+        // {
+        //     path: "reserve-list",
+        //     element: <Suspense fallback={Loading}><ReserveList /></Suspense>
+        // },
 
 
 
