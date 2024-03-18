329acc6999513ee1b29ca96c6674414b50ce3cf4
opjt <jtpark1957@gmail.com>
2024-03-18 19:48:33 +0900

    merge bugfix

--- src/pages/member/user/UserMyPage.js
+++ src/pages/member/user/UserMyPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import BasicLayout from "layouts/BasicLayout";
 import { useSelector } from "react-redux";
 import UserModifyModal from "components/member/user/UserModifyModal";
